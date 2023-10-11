33866d34ea2cf7c639c4d2d2e4eb86ce4db8ac0f
Yange <posterscofield@gmail.com>
2023-10-11 14:18:57 +0200

    Create 58_LengthOfLastWord.cpp

---
+++ 58_LengthOfLastWord.cpp
@@ -0,0 +1,32 @@
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal substring consisting of non-space characters only.
+
+ 
+
+// Example 1:
+
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        bool start = false;
+        int count = 0;
+        for (std::string::reverse_iterator rit=s.rbegin(); rit!=s.rend(); ++rit){
+            if ((*rit == ' ') && (start == false)){
+                continue;
+            } else if((*rit == ' ' ) && (start == true)){
+                break;
+            } else {
+                start = true;
+                count++;
+            }
+
+        }
+        return count;
+
+    } 
+};
