7998ebbebc4f2fcfe3828495fd27681d744886b5
Yange <posterscofield@gmail.com>
2024-01-20 14:08:11 +0100

    Create 036_ValidSudoku.py

---
+++ 036_ValidSudoku.py
@@ -0,0 +1,68 @@
+# Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+# Each row must contain the digits 1-9 without repetition.
+# Each column must contain the digits 1-9 without repetition.
+# Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+# Note:
+
+# A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+# Only the filled cells need to be validated according to the mentioned rules.
+ 
+
+# Example 1:
+
+
+# Input: board = 
+# [["5","3",".",".","7",".",".",".","."]
+# ,["6",".",".","1","9","5",".",".","."]
+# ,[".","9","8",".",".",".",".","6","."]
+# ,["8",".",".",".","6",".",".",".","3"]
+# ,["4",".",".","8",".","3",".",".","1"]
+# ,["7",".",".",".","2",".",".",".","6"]
+# ,[".","6",".",".",".",".","2","8","."]
+# ,[".",".",".","4","1","9",".",".","5"]
+# ,[".",".",".",".","8",".",".","7","9"]]
+# Output: true
+# Example 2:
+
+# Input: board = 
+# [["8","3",".",".","7",".",".",".","."]
+# ,["6",".",".","1","9","5",".",".","."]
+# ,[".","9","8",".",".",".",".","6","."]
+# ,["8",".",".",".","6",".",".",".","3"]
+# ,["4",".",".","8",".","3",".",".","1"]
+# ,["7",".",".",".","2",".",".",".","6"]
+# ,[".","6",".",".",".",".","2","8","."]
+# ,[".",".",".","4","1","9",".",".","5"]
+# ,[".",".",".",".","8",".",".","7","9"]]
+# Output: false
+# Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+
+class Solution:
+
+
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for row in board:
+            if not self.is_valid_group(row):
+                return False
+
+        for column in zip(*board):
+            if not self.is_valid_group(column):
+                return False
+
+        for i in range(0,9,3):
+            for j in range(0,9,3):
+                sub_box = [board[x][y] for x in range (i,i+3) for y in range (j, j+3)]
+                if not self.is_valid_group(sub_box):
+                    return False
+        return True
+
+    def is_valid_group(self,group):
+        seen = set()
+        for num in group:
+            if num != '.':
+                if num in seen:
+                    return False
+                seen.add(num)
+        return True
