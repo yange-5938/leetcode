54c663c2b6aadb6728722228af7865de795bfc34
Yange <posterscofield@gmail.com>
2024-01-28 12:19:44 +0100

    Create 040_CombinationSumII.py

---
+++ 040_CombinationSumII.py
@@ -0,0 +1,44 @@
+# Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+# Each number in candidates may only be used once in the combination.
+
+# Note: The solution set must not contain duplicate combinations.
+
+ 
+
+# Example 1:
+
+# Input: candidates = [10,1,2,7,6,1,5], target = 8
+# Output: 
+# [
+# [1,1,6],
+# [1,2,5],
+# [1,7],
+# [2,6]
+# ]
+# Example 2:
+
+# Input: candidates = [2,5,2,1,2], target = 5
+# Output: 
+# [
+# [1,2,2],
+# [5]
+# ]
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        def backtrack(start, target, path):
+            if target == 0:
+                result.append(path)
+                return
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i] == candidates[i-1]:
+                    continue
+                if candidates[i] > target:
+                    continue
+                backtrack(i+1,target- candidates[i],path+[candidates[i]])
+        candidates.sort()
+        result = []
+        backtrack(0, target, [])
+        return result
+        
