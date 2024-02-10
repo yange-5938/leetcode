b05cc954dce0f0c07ff73b464627c7470b29a757
yange <posterscofield@gmail.com>
2024-02-10 08:40:48 +0000

    modified:   047.permutations-ii.py

--- 047.permutations-ii.py
+++ 047.permutations-ii.py
@@ -7,17 +7,43 @@
 # @lc code=start
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        def backtrack(path,nums):
+        
+        #########################################
+        # A trivial way is to do normal permutation with backtrack
+        # and put all solution into a set, so everything is unique.
+        # 
+        # def backtrack(path,nums):
+        #     if not nums:
+        #         result.add(tuple(path))
+        #         return
+        #     for i in range(len(nums)):
+        #         backtrack(path + [nums[i]], nums[:i] + nums[i+1:])
+        #
+        #
+        # result = set()
+        # backtrack([],nums)
+        # return [list(perm) for perm in result]
+        #
+        ######################################
+    
+
+        # A better way would be sort it first, and then check if the newer
+        # one is same as the last one 
+        
+        def backtrack(path, nums):
             if not nums:
-                result.add(tuple(path))
+                result.append(path[:])
                 return
             for i in range(len(nums)):
-                backtrack(path + [nums[i]], nums[:i] + nums[i+1:])
-        
-        
-        result = set()
-        backtrack([],nums)
-        return [list(perm) for perm in result]
+                if i > 0 and nums[i] == nums[i-1]:
+                    continue
+                backtrack(path+[nums[i]], nums[:i]+nums[i+1:])
+
+        nums.sort()
+        result = []
+        backtrack ([],nums)
+        return result
+
         
 # @lc code=end
 
