322788ec523542e899ffd60753a9bca9bc5366ed
Yange <posterscofield@gmail.com>
2024-05-21 22:30:09 +0200

    Create 707.design-linked-list.py

---
+++ 707.design-linked-list.py
@@ -0,0 +1,102 @@
+# Design your implementation of the linked list. You can choose to use a singly or doubly linked list.
+# A node in a singly linked list should have two attributes: val and next. val is the value of the current node, and next is a pointer/reference to the next node.
+# If you want to use the doubly linked list, you will need one more attribute prev to indicate the previous node in the linked list. Assume all nodes in the linked list are 0-indexed.
+
+# Implement the MyLinkedList class:
+
+# MyLinkedList() Initializes the MyLinkedList object.
+# int get(int index) Get the value of the indexth node in the linked list. If the index is invalid, return -1.
+# void addAtHead(int val) Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+# void addAtTail(int val) Append a node of value val as the last element of the linked list.
+# void addAtIndex(int index, int val) Add a node of value val before the indexth node in the linked list. If index equals the length of the linked list, the node will be appended to the end of the linked list. If index is greater than the length, the node will not be inserted.
+# void deleteAtIndex(int index) Delete the indexth node in the linked list, if the index is valid.
+ 
+
+# Example 1:
+
+# Input
+# ["MyLinkedList", "addAtHead", "addAtTail", "addAtIndex", "get", "deleteAtIndex", "get"]
+# [[], [1], [3], [1, 2], [1], [1], [1]]
+# Output
+# [null, null, null, null, 2, null, 3]
+
+# Explanation
+# MyLinkedList myLinkedList = new MyLinkedList();
+# myLinkedList.addAtHead(1);
+# myLinkedList.addAtTail(3);
+# myLinkedList.addAtIndex(1, 2);    // linked list becomes 1->2->3
+# myLinkedList.get(1);              // return 2
+# myLinkedList.deleteAtIndex(1);    // now the linked list is 1->3
+# myLinkedList.get(1);              // return 3
+
+
+class Node:
+    def __init__(self, val=0, next=None):
+        self.next = next
+        self.val = val
+
+class MyLinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def get(self, index: int) -> int:
+        if index < 0 or index >= self.size:
+            return -1
+        curr = self.head
+        for _ in range(index):
+            curr = curr.next
+        return curr.val
+
+    def addAtHead(self, val: int) -> None:
+        new_node = Node(val, self.head)
+        self.head = new_node
+        self.size += 1
+
+    def addAtTail(self, val: int) -> None:
+        new_node = Node(val)
+        if self.head == None:
+            self.head = new_node
+        else:
+            curr = self.head
+            while(curr.next):
+                curr = curr.next
+            curr.next = new_node
+        self.size += 1
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index < 0 or index > self.size:
+            return
+        new_node = Node(val)
+        if index == 0:
+            self.addAtHead(val)
+        else:
+            temp = self.head
+            for _ in range(index - 1):
+                temp = temp.next
+            new_node.next = temp.next
+            temp.next = new_node
+        self.size += 1
+
+    def deleteAtIndex(self, index: int) -> None:
+        if self.size <= index:
+            return
+        if index == 0:
+            self.head = self.head.next
+        else:
+            temp = self.head
+            for _ in range(index-1):
+                temp = temp.next
+            if temp.next:
+                temp.next = temp.next.next
+        self.size -=1
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
