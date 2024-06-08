de1f28dcb981177e4bdee94c54e5ee76b1f16607
Yange <posterscofield@gmail.com>
2024-06-08 11:23:59 +0200

    Update 0620.not-boring-movies.sql

--- sql/0620.not-boring-movies.sql
+++ sql/0620.not-boring-movies.sql
@@ -45,7 +45,7 @@
 -- We have three movies with odd-numbered IDs: 1, 3, and 5. The movie with ID = 3 is boring so we do not include it in the answer.
 
 # Write your MySQL query statement below
-SELECT *
-FROM Cinema
-WHERE id%2 != 0 and description != 'boring'
-ORDER BY rating desc
+select *
+from Cinema as c
+where (c.id%2 != 0) and c.description != 'boring'
+order by c.rating desc
