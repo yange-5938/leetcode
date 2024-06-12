70b3b631fa97114ca21a7905188a33139ee29986
Yange <posterscofield@gmail.com>
2024-06-12 11:22:47 +0200

    Create 257.binary-tree-paths.py

---
+++ 257.binary-tree-paths.py
@@ -0,0 +1,50 @@
+# Given the root of a binary tree, return all root-to-leaf paths in any order.
+
+# A leaf is a node with no children.
+
+ 
+
+# Example 1:
+
+
+# Input: root = [1,2,3,null,5]
+# Output: ["1->2->5","1->3"]
+# Example 2:
+
+# Input: root = [1]
+# Output: ["1"]
+ 
+
+# Constraints:
+
+# The number of nodes in the tree is in the range [1, 100].
+# -100 <= Node.val <= 100
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        def dfs(node, path, result):
+            if not node:
+                return None
+
+            if len(path)>0:
+                path += "->"
+            path += str(node.val)
+
+            if not node.left and not node.right:
+                result.append(path)
+                return
+            
+            if node.left:
+                dfs(node.left, path, result)
+            if node.right:
+                dfs(node.right , path, result)
+        
+        result = []
+        dfs(root, '', result)
+        return result
