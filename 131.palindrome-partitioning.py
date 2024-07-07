01b38dbe223f3689dfe9e1ef154779a6ce4bfcf7
Yange <posterscofield@gmail.com>
2024-07-07 09:37:42 +0200

    Create 131.palindrome-partitioning.py

---
+++ 131.palindrome-partitioning.py
@@ -0,0 +1,42 @@
+# Given a string s, partition s such that every 
+# substring
+#  of the partition is a 
+# palindrome
+# . Return all possible palindrome partitioning of s.
+
+ 
+
+# Example 1:
+
+# Input: s = "aab"
+# Output: [["a","a","b"],["aa","b"]]
+# Example 2:
+
+# Input: s = "a"
+# Output: [["a"]]
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 16
+# s contains only lowercase English letters.
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        def is_palindrome(sub):
+            return sub == sub[::-1]
+        
+        def backtrack(start, path):
+            if start == len(s):
+                result.append(path[:])
+                return
+            for end in range(start+1,len(s)+1):
+                prefix = s[start:end]
+                if is_palindrome(prefix):
+                    path.append(prefix)
+                    backtrack(end,path)
+                    path.pop()
+        
+        result = []
+        backtrack(0, [])
+        return result
